f26117b11bc87a08938317446976c2e039102820
Kyle Joseph Nadeau <kylejnadeau@gmail.com>
2024-11-11 07:59:15 -0500

    Generate test window

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,50 @@
 
 int main () {
 
+    //Intitalize GLFW
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << '\n';
+        return -1;
+    }
+
+    //Use OpenGL Version 4.1 (Highest version usable on MacOS)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    //Create test window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello Window!", NULL, NULL);
+    if(!window) {
+        std::cerr << "Failed to create window" << '\n';
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    
+    //Intitalize Glad
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cerr << "Failed to initialize GLAD" << '\n';
+    return -1;
+    }
+
+    //Simple Check
     std::cout << "Hello World!";
     std::cout << '\n' << "CMake Compilation" << '\n';
+
+    //Main loop
+    while (!glfwWindowShouldClose(window)) {
+        // Render here
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+
+        // Poll for and process events
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
