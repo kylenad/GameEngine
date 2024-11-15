68f6b5e018522fd7fcd807875f01efcf70396d93
Kyle Joseph Nadeau <kylejnadeau@gmail.com>
2024-11-15 14:33:29 -0500

    updated shaders, more in/out potential

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,19 @@ void processInput(GLFWwindow *window);
 //Basic vertex shader definition using version 4.1
 const char *vertexShaderSource = "#version 410 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "out vec4 vertexColor;\n"
     "void main()\n"
     "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n" //orange
+    "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n" 
+    "vertexColor = vec4(1.0, 0.0, 0.0, 1.0);\n"
     "}\0";
 
 //Fragment shader definition
 const char *fragmentShaderSource = "#version 410 core\n"
     "out vec4 FragColor;\n"
+    "in vec4 vertexColor;\n"
     "void main() {\n"
-    "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "FragColor = vertexColor;\n"
     "}\0";
 const char *fragmentShaderSource2 = "#version 410 core\n"
     "out vec4 FragColor;\n"
